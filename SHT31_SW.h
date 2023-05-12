5f687af2c8c5a328736475111e363ff56a63579e
Rob Tillaart <rob.tillaart@gmail.com>
2023-05-12 11:59:07 +0200

    add SoftwareWire version

--- SHT31_SW.h
+++ SHT31_SW.h
@@ -12,12 +12,12 @@
 //     URL: https://github.com/RobTillaart/SHT31
 
 
+#define SHT31_SW_LIB_VERSION             (F("0.1.1"))
+
+
 #include "Arduino.h"
 #include "SoftWire.h"
 #include "SHT31.h"
-
-
-#define SHT31_SW_LIB_VERSION             (F("0.1.1"))
 
 
 class SHT31_SW : public SHT31
@@ -37,6 +37,44 @@ private:
   bool readBytes(uint8_t n, uint8_t *val);
   SoftWire* _softWire;
 };
+
+
+
+/////////////////////////////////////////////////////////
+//
+//  This version is using an AVR specific SoftwareWire
+//  it can read the SHT85 where the above can't.
+//  See https://github.com/RobTillaart/SHT31_SW/issues/5
+//
+//  you need to comment the above version (also in .cpp file)
+//  and use SoftwareWire instead of SoftWire in your project.
+//
+
+
+/*
+#include "Arduino.h"
+#include "SoftwareWire.h"
+#include "SHT31.h"
+
+
+class SHT31_SW : public SHT31
+{
+public:
+  SHT31_SW();
+
+  //  use SHT_DEFAULT_ADDRESS
+  bool begin(const uint8_t address,  SoftwareWire *wire);
+  bool begin(SoftwareWire *wire);
+
+  //  check sensor is reachable over I2C
+  bool isConnected();
+
+private:
+  bool writeCmd(uint16_t cmd);
+  bool readBytes(uint8_t n, uint8_t *val);
+  SoftwareWire* _softWire;
+};
+*/
 
 
 //  -- END OF FILE --
